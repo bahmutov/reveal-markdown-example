92d834a6d7b4e0688583d8fe59bc84b359fce75c
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2021-02-16 11:41:28 -0500

    load markdown dynamically

--- main.js
+++ main.js
@@ -3,7 +3,14 @@ import 'reveal.js/dist/theme/league.css'
 import Reveal from 'reveal.js'
 import Markdown from 'reveal.js/plugin/markdown/markdown.esm.js'
 
-let deck = new Reveal({
+const markdownFilename = './slides/intro/PITCHME.md'
+document.querySelector('.slides').innerHTML = `
+  <section data-markdown="${markdownFilename}"
+           data-separator="\\-\\-\\-"
+           data-separator-vertical="\\+\\+\\+" />
+`
+
+const deck = new Reveal({
   plugins: [Markdown],
 })
 deck.initialize({ slideNumber: true, minScale: 0.2, maxScale: 1.1 })

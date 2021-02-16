6df590710f94a7b2793683646c32bf03bba52a77
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2021-02-16 11:36:14 -0500

    working

--- main.js
+++ main.js
@@ -1,10 +1,9 @@
 import 'reveal.js/dist/reveal.css'
 import 'reveal.js/dist/theme/league.css'
-
 import Reveal from 'reveal.js'
 import Markdown from 'reveal.js/plugin/markdown/markdown.esm.js'
 
 let deck = new Reveal({
   plugins: [Markdown],
 })
-deck.initialize({ slideNumber: true })
+deck.initialize({ slideNumber: true, minScale: 0.2, maxScale: 1.1 })
